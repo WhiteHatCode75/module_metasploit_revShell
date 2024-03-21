16233ea42d20e8803bf6d3c5f760a437f9ebb3fa
WhiteHatCode75 <lm3904@yahoo.fr>
2024-03-21 12:54:21 +0100

    [ADD] commands, register options

--- finder_files_by_extension.rb
+++ finder_files_by_extension.rb
@@ -8,15 +8,38 @@ class MetasploitModule < Msf::Post
     super(
       update_info(
         info,
-        'Name' => '[Windows] [Post] [Listener] [Get all files with txt extension and enumerate]',
+        'Name' => '[Platform] [Post] [Listener] [Get all files with txt extension and enumerate]',
         'Description' => %q{
           Say something that the user might want to know.
         },
         'License' => MSF_LICENSE,
         'Author' => [ 'Adil et Lazare' ],
         'Platform' => [ 'win', 'linux',],
-        'SessionTypes' => [ 'meterpreter']
+        'SessionTypes' => [ 'meterpreter'],
+        'Compat' => {
+          'Meterpreter' => {
+            'Commands' => %w[
+              core_migrate
+              stdapi_railgun_api
+              stdapi_sys_process_get_processes
+              stdapi_sys_process_getpid
+              stdapi_ui_get_idle_time
+              stdapi_ui_get_keys_utf8
+              stdapi_ui_start_keyscan
+              stdapi_ui_stop_keyscan
+            ]
+          }
+        }
       )
+    )
+
+    register_options(
+      [
+        OptString.new('EXTENSION', [true, 'File Extension to harvest', 'txt']),
+        OptPath.new('DIRECTORY', [true, 'Starting point', '/']),
+        OptBool.new('VERBOSE', [false, 'verbose mode', false]),
+        OptString.new('FILE NAME', [false, 'search a specific file', nil]),
+      ]
     )
   end
 

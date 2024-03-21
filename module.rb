a2082328d365e97840ab57b8261539f432321cd6
WhiteHatCode75 <lm3904@yahoo.fr>
2024-03-21 12:05:46 +0100

    tesy

--- module.rb
+++ module.rb
@@ -1,89 +1,26 @@
-require 'socket'
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
 
-class MetasploitModule < Msf::Exploit::Remote
-    Rank = NormalRanking
-  
-    def initialize(info = {})
-      super(
-        update_info(
-          info,
-          'Name' => '[Upec] [Software] [Root Cause] [Vulnerability type]',
-          'Description' => %q{
-            reverse_shell manager by lazare and adil
-          },
-          'License' => MSF_LICENSE,
-          'Author' => [ 'Name' ],
-          'References' => [
-            [ 'URL', '' ]
-          ],
-          'Platform' => 'win',
-          'Targets' => [
-            [
-              'Windows x64',
-              {
-                'Ret' => 0x41414141 # This will be available in `target.ret`
-              }
-            ]
-          ],
-          'Payload' => {
-            'BadChars' => "\x00"
-          },
-          'Privileged' => false,
-          'DisclosureDate' => '',
-          'DefaultTarget' => 0,
-          'Notes' => {
-            'Stability' => [CRASH_SAFE],
-            'Reliability' => [REPEATABLE_SESSION],
-            'SideEffects' => [ARTIFACTS_ON_DISK, IOC_IN_LOGS]
-          },
-        )
+class MetasploitModule < Msf::Post
+  def initialize(info = {})
+    super(
+      update_info(
+        info,
+        'Name' => '[Platform] [Module Category] [Software] [Function]',
+        'Description' => %q{
+          Say something that the user might want to know.
+        },
+        'License' => MSF_LICENSE,
+        'Author' => [ 'Name' ],
+        'Platform' => [ 'win', 'linux', 'osx', 'unix', 'bsd', 'solaris' ],
+        'SessionTypes' => [ 'meterpreter', 'shell' ]
       )
-    end
-  
-    def check
-
-    end
-  
-    def exploit
-      
-    end
-
-    def initServer      
-      # Création d'un serveur TCP sur le port 8888
-      server = TCPServer.new(8888)
-
-      # Tableau pour stocker les clients connectés
-      clients = []
-
-      # Fonction pour envoyer un message à tous les clients
-      def broadcast(message, clients)
-        clients.each do |client|
-          client.puts message
-        end
-      end
-
-      # Accepter et gérer les connexions des clients
-      Thread.new do
-        loop do
-          client = server.accept
-          clients << client
-
-          # Lecture des messages du client et diffusion à tous les clients
-          Thread.new(client) do |cl|
-            loop do
-              message = cl.gets.chomp
-              broadcast(message, clients)
-            end
-          end
-        end
-      end
-
-      # Lire les saisies de l'utilisateur et les envoyer à tous les clients
-      loop do
-        print "Saisir un message : "
-        message = gets.chomp
-        broadcast(message, clients)
-      end
-    end  
-  
+    )
   end
+
+  def run
+    # Main method
+  end
+end
